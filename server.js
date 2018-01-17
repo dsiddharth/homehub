7185f83feb0f2f284b86f493638d048f2daba1ba
Siddharth Dhulipalla <dsiddharth@gmail.com>
2018-01-16 21:42:19 -0800

    add systemd config

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ app.get('/volume/:vol', function (req, res) {
 	res.send(req.params.vol)
 	exec('onkyo volume='+req.params.vol)
 })
-
+app.get('/source/:src', function (req, res) {
+	res.send(req.params.src)
+	console.log(req.params)
+	exec('onkyo source='+req.params.src.replace(/ /g, ''))
+})
 app.listen(4000, function () {
 	  console.log('Example app listening on port 4000!')
 })
